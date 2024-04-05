c11c6fb6e07a169e18678cd89b20be07d55fb1a7
nicol <nicolas_biletzki@hotmail.com>
2024-04-04 21:45:17 -0300

    Adicionando EFC e Contexto

---
+++ SmartSchool.WebAPI/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartSchool.WebAPI.Models;
+using System.Data.Common;
+
+namespace SmartSchool.WebAPI.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+                
+        }
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Professor> Professores { get; set; }
+        public DbSet<Disciplina> Disciplinas { get; set; }
+        public DbSet<AlunoDisciplina> AlunosDisciplinas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<AlunoDisciplina>().HasKey(AD => new { AD.AlunoId, AD.DisciplinaId });
+        }
+    }
+}
